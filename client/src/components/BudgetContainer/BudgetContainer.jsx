1a57c7b8d025f4c3cf60ad50d3ac9229ebc7d6ee
Cybre3 <cybre3@gmail.com>
2025-06-21 17:58:48 -0400

    removed warnings and new build

--- client/src/components/BudgetContainer/BudgetContainer.jsx
+++ client/src/components/BudgetContainer/BudgetContainer.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import Joi from 'joi-browser';
+import React from "react";
+import Joi from "joi-browser";
 
-import Form from '../common/form';
-import calculateBudget from '../../utils/calculations';
-import BillsBreakdown from './BillsBreakdown';
-import { FIRST_HALF_MONTH_BILL_TOTAL, SECOND_HALF_MONTH_BILL_TOTAL } from './billInfo';
-import BankAccounts from './BankAccounts';
+import Form from "../common/form";
+import calculateBudget from "../../utils/calculations";
+// import BillsBreakdown from "./BillsBreakdown";
+// import {
+//   FIRST_HALF_MONTH_BILL_TOTAL,
+//   SECOND_HALF_MONTH_BILL_TOTAL,
+// } from "./billInfo";
+// import BankAccounts from "./BankAccounts";
+// import Input from "./../common/input";
 
 class BudgetContainer extends Form {
   state = {
@@ -24,7 +28,6 @@ class BudgetContainer extends Form {
       cash: 0,
     },
     errors: {},
-   
   };
 
   componentDidMount() {
@@ -44,28 +47,28 @@ class BudgetContainer extends Form {
 
   schema = {
     _id: Joi.string(),
-    income: Joi.number().required().label('Income'),
-    tithe: Joi.number().required().label('Tithe Percent'),
-    titheAmount: Joi.number().required().label('Tithe Amount'),
-    bills: Joi.number().required().label('Bills'),
-    additionalFunds: Joi.number().required().label('additionalFunds'),
-    savingsPercentage: Joi.number().required().label('savingsPercentage'),
-    emergeSavePercentage: Joi.number().required().label('emergeSavePercentage'),
-    gas: Joi.number().required().label('gas'),
-    house: Joi.number().required().label('house'),
-    food: Joi.number().required().label('food'),
-    meds: Joi.number().required().label('meds'),
-    give: Joi.number().required().label('give'),
-    electronicGive: Joi.number().required().label('Electronic Give'),
-    cash: Joi.number().required().label('cash'),
+    income: Joi.number().required().label("Income"),
+    tithe: Joi.number().required().label("Tithe Percent"),
+    titheAmount: Joi.number().required().label("Tithe Amount"),
+    bills: Joi.number().required().label("Bills"),
+    additionalFunds: Joi.number().required().label("additionalFunds"),
+    savingsPercentage: Joi.number().required().label("savingsPercentage"),
+    emergeSavePercentage: Joi.number().required().label("emergeSavePercentage"),
+    gas: Joi.number().required().label("gas"),
+    house: Joi.number().required().label("house"),
+    food: Joi.number().required().label("food"),
+    meds: Joi.number().required().label("meds"),
+    give: Joi.number().required().label("give"),
+    electronicGive: Joi.number().required().label("Electronic Give"),
+    cash: Joi.number().required().label("cash"),
   };
 
   inputClasses = {
-    inputContainer: 'col-span-full',
+    inputContainer: "col-span-full",
   };
 
-  buttonContainerClass = 'col-span-full justify-self-center pt-6';
-  btnClass = 'cursor-pointer bg-neutral-600 py-1 px-4 rounded-md';
+  buttonContainerClass = "col-span-full justify-self-center pt-6";
+  btnClass = "cursor-pointer bg-neutral-600 py-1 px-4 rounded-md";
 
   handleAccountChanges = (accounts) => {
     this.setState({ accounts });
@@ -83,9 +86,15 @@ class BudgetContainer extends Form {
 
     return (
       <div className="bg-neutral-700 text-white w-1/2 mx-auto py-4 rounded-md relative">
-        <h1 className="text-center text-xl uppercase font-bold my-4">Name Budget {new Date().toLocaleString('en-us', { year: 'numeric', month: 'long'})}</h1>
+        <h1 className="text-center text-xl uppercase font-bold my-4">
+          Name Budget{" "}
+          {new Date().toLocaleString("en-us", {
+            year: "numeric",
+            month: "long",
+          })}
+        </h1>
         <form className="bg-black w-3/4 mx-auto p-4 grid grid-cols-2 auto-rows-auto gap-4 gap-x-4 rounded-md">
-          {this.renderInput('income', 'Income')}
+          {this.renderInput("income", "Income")}
 
           <div className="flex space-x-4">
             <h3>Tithe</h3>
@@ -93,7 +102,7 @@ class BudgetContainer extends Form {
           </div>
 
           <div>
-            {this.renderInput('bills', 'Bills')}
+            {this.renderInput("bills", "Bills")}
             {/* <BillsBreakdown /> */}
           </div>
 
@@ -101,22 +110,21 @@ class BudgetContainer extends Form {
             <h3>After Tithe & Bills</h3>
             <span>$ {income_after_titheBills}</span>
             {this.renderInput(
-              'additionalFunds',
-              'Additional Funds',
-              'text',
+              "additionalFunds",
+              "Additional Funds",
+              "text",
               false,
               this.inputClasses
             )}
           </div>
 
           <div className="bg-slate-700 p-2 space-y-2 rounded-md border border-white">
-
             <div className="grid grid-rows-2">
               <div className="flex space-x-2">
                 <h3>Emergency Savings</h3>
                 <span>$ {emergeSaveAmount}</span>
               </div>
-              {this.renderInput('emergeSavePercentage', 'Emerge Savings %')}
+              {this.renderInput("emergeSavePercentage", "Emerge Savings %")}
             </div>
 
             <div className="grid grid-rows-2">
@@ -124,9 +132,8 @@ class BudgetContainer extends Form {
                 <h3>Savings</h3>
                 <span>$ {savingsAmount}</span>
               </div>
-              {this.renderInput('savingsPercentage', 'Savings %')}
+              {this.renderInput("savingsPercentage", "Savings %")}
             </div>
-            
           </div>
 
           <div className="justify-self-center self-center text-center">
@@ -135,12 +142,50 @@ class BudgetContainer extends Form {
           </div>
 
           <div className="bg-slate-700 p-2 rounded-md space-y-1 border border-white">
-            {this.renderInput('gas', 'Gas')}
-            {this.renderInput('house', 'House')}
-            {this.renderInput('food', 'Food')}
-            {this.renderInput('meds', 'Medicine')}
-            {this.renderInput('cash', 'Cash')}
-            <button className='bg-gray-400 p-1 px-3 text-black border-black border rounded'>add category</button>
+            <div className="flex place-content-evenly">
+              <input
+                name="Gas"
+                placeholder="Gas"
+                className="border-0 border-transparent outline-0 placeholder-white bg-transparent w-fit text-white px-2"
+              />{" "}
+              {this.renderInput("gas", "")}
+            </div>
+            <div className="flex">
+              <input
+                name="House"
+                placeholder="House"
+                className="border-0 border-transparent outline-0 placeholder-white bg-transparent"
+              />{" "}
+              {this.renderInput("house", "")}
+            </div>
+            <div className="flex">
+              <input
+                name="Food"
+                placeholder="Food"
+                className="border-0 border-transparent outline-0 placeholder-white bg-transparent"
+              />{" "}
+              {this.renderInput("food", "")}
+            </div>
+            <div className="flex">
+              <input
+                name="Medicine"
+                placeholder="Medicine"
+                className="border-0 border-transparent outline-0 placeholder-white bg-transparent"
+              />{" "}
+              {this.renderInput("meds", "")}
+            </div>
+            <div className="flex">
+              <input
+                name="Cash"
+                placeholder="Cash"
+                className="border-0 border-transparent outline-0 placeholder-white bg-transparent"
+              />{" "}
+              {this.renderInput("cash", "")}
+            </div>
+
+            <button className="bg-gray-400 p-1 px-3 text-black border-black border rounded">
+              add category
+            </button>
           </div>
 
           <div className="justify-self-center self-center text-center">
@@ -148,13 +193,12 @@ class BudgetContainer extends Form {
             <span>$ {cash_left}</span>
           </div>
 
-          {this.renderButton('SAVE', this.buttonContainerClass, this.btnClass)}
-        </form> 
+          {/* {this.renderButton("SAVE", this.buttonContainerClass, this.btnClass)} */}
+        </form>
 
         <hr className="my-10 mb-12" />
 
         {/* <BankAccounts /> */}
-      
       </div>
     );
   }
